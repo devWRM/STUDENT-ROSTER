1f471862ee3bf1eb3a4d415e0bb75ef7ac176295
devWRM <mcwen@comcast.net>
2021-01-14 13:12:11 -0500

    mounted StudentsForm

--- frontend-student-roster/src/components/StudentsContainer.js
+++ frontend-student-roster/src/components/StudentsContainer.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchStudents } from '../actions/studentsActions.js';
+import StudentsForm from './StudentsForm.js';
 
 class StudentsContainer extends Component {
 
     componentDidMount() {
-        // console.log(this)            // <<= StudentsContainer
-        // console.log(this.props)      // <<= {fetchStudents: f}
-        // this.props()                 // <<= TypeError: this.props is not a function
-        // this.props.fetchStudents     // <<= nothing
-        this.props.fetchStudents()      // <<= Got it! (4) [{…}, {…}, {…}, {…}]
+        this.props.fetchStudents()      // <<= datastudents: (4) [{…}, {…}, {…}, {…}]
     }
 
     render() {
         return (
             <div>
                 This is the StudentsContainer
+                <StudentsForm />
             </div>
         );
     }
