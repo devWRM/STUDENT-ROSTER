609d38c4bc91526f9d4c68652d6e811e3747dede
devWRM <mcwen@comcast.net>
2021-01-14 10:56:23 -0500

    created skeleton of StudentsForm class (rcc + tab) & moved export default to the bottom of the page

---
+++ frontend-student-roster/src/components/StudentsForm.js
@@ -0,0 +1,23 @@
+// Sets component (local) state
+
+
+
+import React, { Component } from 'react'
+
+class StudentsForm extends Component {
+
+    state = {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default StudentsForm;
