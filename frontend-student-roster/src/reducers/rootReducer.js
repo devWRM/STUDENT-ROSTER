c00cd9314c87d9c830d5ceec9f884ba8936b5e11
devWRM <mcwen@comcast.net>
2021-01-16 12:43:35 -0500

    connected word store to the React app

--- frontend-student-roster/src/reducers/rootReducer.js
+++ frontend-student-roster/src/reducers/rootReducer.js
@@ -3,11 +3,13 @@
 import { combineReducers } from 'redux';
 
 import { studentsReducer } from './studentsReducer.js';
+import { wordsReducer } from './wordsReducer.js';
 
 
 
 export const rootReducer = combineReducers({
     // key sent to store can be named anything ex. students, pupils
     // value calls specific reducer to update state
-    students: studentsReducer
+    students: studentsReducer,
+    words: wordsReducer
 })
---
+++ frontend-student-roster/src/reducers/wordsReducer.js
@@ -0,0 +1,13 @@
+export const wordsReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_STUDENT_WORDS':
+            // return action.payload
+        case 'NEW_WORD':
+            // return [...state, action.payload]
+        case 'DELETE_WORD':
+            // let newStudents = state.filter(student => student.id !== action.payload)
+            // return [...newStudents] 
+        default:
+            return state
+    }
+}
