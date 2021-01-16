5765e392a2c77816c782a736d85edaa73351ee8c
devWRM <mcwen@comcast.net>
2021-01-16 13:25:00 -0500

    started building the wordsReducer arrow function

--- frontend-student-roster/src/reducers/wordsReducer.js
+++ frontend-student-roster/src/reducers/wordsReducer.js
@@ -1,9 +1,9 @@
 export const wordsReducer = (state = [], action) => {
     switch(action.type) {
         case 'FETCH_STUDENT_WORDS':
-            // return action.payload
+            return action.payload
         case 'NEW_WORD':
-            // return [...state, action.payload]
+            return [...state, action.payload]
         case 'DELETE_WORD':
             // let newStudents = state.filter(student => student.id !== action.payload)
             // return [...newStudents] 
