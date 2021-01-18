99e55e67e6ec4c8067a8af75eefdda9008b33d7b
devWRM <mcwen@comcast.net>
2021-01-17 21:25:18 -0500

    saved orig App.js file for comparison

---
+++ zApp.js
@@ -0,0 +1,24 @@
+// PROJECT REQUIREMENT: Stateless Component
+
+
+import React from 'react';
+
+// Replaced with Routes from Router
+// import StudentsContainer from './StudentsContainer.js';
+
+import Router from './Router.js';
+
+
+const App = () => {
+    return(
+        <div>
+            This is App.js
+            {/* <StudentsContainer /> */}
+
+            <Router />
+        </div>
+    );
+}
+
+
+export default App;
