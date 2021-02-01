fb3a58462587a80ed0203203e0133044d6a2b8d7
devWRM <mcwen@comcast.net>
2021-02-01 08:29:00 -0500

    added word sentence for display

--- frontend-student-roster/src/components/word-components/Word.js
+++ frontend-student-roster/src/components/word-components/Word.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Word(props) {
     return (
         <div>
-            {props.word.spelling}
+            {props.word.spelling}<br></br>
+            {props.word.sentence}
         </div>
     )
 }
