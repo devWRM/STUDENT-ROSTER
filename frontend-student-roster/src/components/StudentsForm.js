7b6637772b8cbbe9b779dce2cd58fd2053f1477e
devWRM <mcwen@comcast.net>
2021-02-01 10:06:37 -0500

    added instructions to create a new student

--- frontend-student-roster/src/components/StudentsForm.js
+++ frontend-student-roster/src/components/StudentsForm.js
@@ -40,18 +40,21 @@ class StudentsForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>Name</label>
-                <input type="text" value={this.state.name} onChange={this.handleChange} name="name"></input><br></br><br></br>
+            <div>
+                <p>Fill in the boxes below then click the Create Student button to add a new student to the list.</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name</label>
+                    <input type="text" value={this.state.name} onChange={this.handleChange} name="name"></input><br></br><br></br>
 
-                <label>Nickname</label>
-                <input type="text" value={this.state.nickname} onChange={this.handleChange} name="nickname"></input><br></br><br></br>
+                    <label>Nickname</label>
+                    <input type="text" value={this.state.nickname} onChange={this.handleChange} name="nickname"></input><br></br><br></br>
 
-                <label>Email</label>
-                <input type="text" value={this.state.email} onChange={this.handleChange} name="email"></input><br></br><br></br>
+                    <label>Email</label>
+                    <input type="text" value={this.state.email} onChange={this.handleChange} name="email"></input><br></br><br></br>
 
-                <input type="submit" value="Create Student"></input><br></br>
-            </form>
+                    <input type="submit" value="Create Student"></input><br></br>
+                </form>
+            </div>
         );
     }
 
