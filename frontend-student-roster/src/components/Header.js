6569fe7554212c608c336967ea82a1d38add49ee
devWRM <mcwen@comcast.net>
2021-02-01 11:10:55 -0500

    added mapStateToProps

--- frontend-student-roster/src/components/Header.js
+++ frontend-student-roster/src/components/Header.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+
+    handleClickHome = () => {
+        this.props.history.push("/")
+    };
+
+    handleClickStudentList = () => {
+        this.props.history.push("/students")
+    };
+
+
     render() {
         return (
             <div>
                  <p>STUDENT ROSTER</p>
-                    <button>Home</button>
-                    <button>Student List</button>
+                    <button onClick={this.handleClickHome}>Home</button>
+                    <button onClick={this.handleClickStudentList}>Student List</button>
             </div>
         )
     }
 }
 
-export default Header
+
+const mapStateToProps = state => {
+    return { students: state.students }
+}
+
+export default connect(mapStateToProps)(withRouter(Header));
