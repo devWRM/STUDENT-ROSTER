b7c9276307011baff37ca9dec74c2b3ce832a180
devWRM <mcwen@comcast.net>
2021-02-01 13:18:46 -0500

    refactored to use match, provided by clicking Link, to find and store the appropriate student in currentStudent

--- frontend-student-roster/src/components/Student.js
+++ frontend-student-roster/src/components/Student.js
@@ -1,6 +1,5 @@
 // Shows a single student and a list of that student's words
 // Functional component because it's just for show & not doing anything to current state
-// Should be similar to StudentsList.js
 
 import React from 'react'
 import { connect } from 'react-redux';
@@ -8,20 +7,25 @@ import { connect } from 'react-redux';
 
 import WordsContainer from './word-components/WordsContainer.js'
 
+
 const Student = (props) => {
 
-
+    const currentStudent = props.students.find(student =>
+        student.id == props.match.params.id
+    )
     // let student = props.students[props.match.params.id - 1]
 
     return (
         <div>
             
-            {/* {student ? student.nickname : null} */}          
-            STUDENT: {props.student.nickname} <br></br> {props.student.name} <br></br>email: {props.student.email} 
-            {/* <br></br> <button onClick={() => this.props.deleteStudent(student.id)}>Delete Student</button> */}
+            {/* STUDENT: {props.student.nickname} <br></br> {props.student.name} <br></br>email: {props.student.email}  */}
 
+            Nickname: {currentStudent.nickname}<br></br>
+            Name: {currentStudent.name}<br></br>
+            Email: {currentStudent.email}
 
-            <WordsContainer student={props.student} />
+            <WordsContainer student={currentStudent} />
+            {/* <WordsContainer student={props.student} /> */}
 
 
 
