eb831421ae6bcb61f17b393f838200c66cff1509
devWRM <mcwen@comcast.net>
2021-02-14 10:03:24 -0500

    added styling to StudentsList.js

--- frontend-student-roster/src/components/StudentsList.js
+++ frontend-student-roster/src/components/StudentsList.js
@@ -6,6 +6,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 
+
+
+
 // https://stackoverflow.com/questions/61906909/cant-understand-why-onclick-not-working-in-react
 // You can't dispatch directly from a HTML element onClick handler. If you bind the redux action creators it should work as expected
 // import { bindActionCreators } from 'redux'      //  <<= works with mapDispatchToProps(dispatch)
@@ -17,6 +20,16 @@ import { deleteStudent } from '../actions/studentsActions.js';
 // import Student from './Student.js';
 
 // refer to as props.students =>>   const StudentsList = () => {
+
+
+    import styled from 'styled-components';
+    const Button = styled.button`
+        background-color: #004d99;
+        color: white;
+        width: 120px;
+        padding: 5px;
+        margin-right: 1rem;
+    `
 
 // destructured to refer to as students
 // const StudentsList = ({ students }) => {
@@ -31,8 +44,17 @@ class StudentsList extends React.Component {
                     <StudentsForm />
 
                     {this.props.students.length ? this.props.students.map(student => 
-                    <div key={student.id}>  
-                        <button onClick={() => this.props.deleteStudent(student.id)}>Delete {student.nickname}</button>  
+                    <div key={student.id}
+                    
+                    style = {{
+                        border: '2px solid #ccc',
+                        padding: '1.5rem'
+                        
+                    }}
+                    
+                    >
+
+                        <Button onClick={() => this.props.deleteStudent(student.id)}><b>Delete {student.nickname}</b></Button>  
                         <Link to={`/students/${student.id}`}>{student.name}</Link>  <br></br>  
                     </div>) : <h3>Roster is empty: No Students</h3>}
 
