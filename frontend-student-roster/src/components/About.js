334c2d8502ba141501076b6ebd1334b50cb658e3
devWRM <mcwen@comcast.net>
2021-02-14 09:39:24 -0500

    added styling and content describing what the Student Roster app can do

--- frontend-student-roster/src/components/About.js
+++ frontend-student-roster/src/components/About.js
@@ -4,10 +4,26 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            About Student Roster
-                <li>lists all students</li>
-                <li>lists vocabulary words for each student</li>
+        <div
+        
+            style = {{
+                padding: '5rem'
+            }}
+
+        >
+
+            <h3>ALL ABOUT STUDENT ROSTER</h3>
+            <h4>Student Roster lets you:</h4>
+            <ul>
+                <li>see a list of students</li>
+                <li>create a new student</li>
+                <li>delete a student (including the student's word list)</li>
+                <li>see a list of words for a specific student</li>
+                <li>create a word for a specific student</li>
+                <li>delete a word</li>
+            </ul>
+
+
         </div>
     );
 }
