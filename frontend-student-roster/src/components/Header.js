ebd8a5fc64778eb08f9c396e9f9a3b8a231a979a
devWRM <mcwen@comcast.net>
2021-02-14 09:51:45 -0500

    styled the 3 buttons using styled components, added the handleClickAbout arrow function & About button

--- frontend-student-roster/src/components/Header.js
+++ frontend-student-roster/src/components/Header.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
 
 
+import './Header.css';
+
+import styled from 'styled-components';
+const Button = styled.button`
+    background-color: white;
+    color: #004d99;
+    width: 100px;
+    padding: 5px;
+    
+`
+
+
+// SLACK Dec 1, 2020: Corrina Brock Moore to Muazzam Nashat:
+// You can use a push state to show a different URL in the browser.
 
 class Header extends Component {
 
     handleClickHome = () => {
         this.props.history.push("/")
+    };
+
+    handleClickAbout = () => {
+        this.props.history.push("/about")
     };
 
     handleClickStudentList = () => {
@@ -16,28 +34,15 @@ class Header extends Component {
 
     render() {
         return (
-            <div
-            
-            style = {{
-                minHeight: '20vh',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'space-around',
-                fontSize: '25px',
-                backgroundColor: '#99e6ff',
-                color: 'black'
+            <div>
+                 <p className="roster-header" style = {{ fontFamily: 'arial', fontSize: '30px', color: '#004d99' }}><b>STUDENT ROSTER</b></p>
 
+                    <div className="roster-buttons">
+                        <Button onClick={this.handleClickHome}><b>Home</b></Button>
+                        <Button onClick={this.handleClickAbout}><b>About</b></Button>
+                        <Button onClick={this.handleClickStudentList}><b>Student List</b></Button>
+                    </div>
 
-                // justifyContent: 'left',
-                // fontSize: '20px',
-                // padding: '1rem'
-            }}
-            
-            >
-                 <p>STUDENT ROSTER</p>
-                    <button onClick={this.handleClickHome}>Home</button>
-                    <button onClick={this.handleClickStudentList}>Student List</button>
             </div>
         )
     }
